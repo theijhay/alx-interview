fb79d4c9053d8d935407fa46e462a4f716f1d98a
theijhay <olawaleisaacjohn@gmail.com>
2024-04-24 17:09:41 +0100

    Pascals Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,18 +10,17 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Start with the first row
+    triangle = [[1]]
 
     for i in range(1, n):
-        prev_row = triangle[-1]  # Get the previous row
-        current_row = [1]  # Each row starts with 1
+        prev_row = triangle[-1]
+        current_row = [1]
 
-        # Calculate the values for the current row
         for j in range(1, i):
             value = prev_row[j - 1] + prev_row[j]
             current_row.append(value)
 
-        current_row.append(1)  # Each row ends with 1
+        current_row.append(1)
         triangle.append(current_row)
 
     return triangle
