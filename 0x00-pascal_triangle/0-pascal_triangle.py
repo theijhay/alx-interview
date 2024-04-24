36e18a22240a7df70bfccc38565a05eba863900d
theijhay <olawaleisaacjohn@gmail.com>
2024-04-24 17:35:34 +0100

    Pascals Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
+
 """Pascal Triangle"""
 
 def pascal_triangle(n):
-    """returns a pascal triangle of n rows"""
-    
+    """
+    Returns a list of lists representing the Pascal's triangle up to n rows.
+    Returns an empty list if n <= 0.
+    """
     if n <= 0:
-        return []  # Return an empty list if n is less than or equal to 0
+        return []
 
-    pascal = [0] * n  # Create a list of n zeros to represent the triangle
+    pascal = [0] * n
 
     for i in range(n):
-        # Create a new row for each iteration
-        nrow = [0] * (i + 1)  # Initialize the new row with zeros
-
-        # Set the first and last element of the row to 1
+        # include the first row
+        nrow = [0] * (i+1)
         nrow[0] = 1
-        nrow[-1] = 1
+        nrow[len(nrow) - 1] = 1
 
-        # Calculate the elements in between using the previous row
         for j in range(1, i):
             if j > 0 and j < len(nrow):
-                a = pascal[i - 1][j]  # Get the value from the previous row
-                b = pascal[i - 1][j - 1]  # Get the value from the previous row and previous column
-                nrow[j] = a + b  # Calculate the current element based on the values above
+                a = pascal[i - 1][j]
+                b = pascal[i - 1][j - 1]
+                nrow[j] = a + b
 
-        pascal[i] = nrow  # Update the triangle with the new row
+        pascal[i] = nrow
 
-    return pascal  # Return the generated Pascal's Triangle
+    return pascal
