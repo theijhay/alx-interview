51af177be2943460ca283f5e74ae4a385df553c3
theijhay <olawaleisaacjohn@gmail.com>
2024-05-02 15:46:56 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,11 +4,19 @@ contains the function canUnlockAll"""
 
 
 def canUnlockAll(boxes):
-    keys = [0]
-    for key in keys:
-        for box in boxes[key]:
-            if box not in keys and box < len(boxes):
-                keys.append(box)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    n = len(boxes)
+    unlocked = {0}  # Set of unlocked box indices
+    keys = set(boxes[0])  # Set of available keys from the first box
+
+    def unlock_boxes(keys):
+        new_keys = set()
+        for key in keys:
+            if key < n and key not in unlocked:
+                unlocked.add(key)
+                new_keys.update(boxes[key])
+        return new_keys
+
+    while keys:
+        keys = unlock_boxes(keys)
+
+    return len(unlocked) == n
