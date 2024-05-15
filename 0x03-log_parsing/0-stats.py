731dca321fac1d35a7a019f51bc63178dc594b78
theijhay <olawaleisaacjohn@gmail.com>
2024-05-15 04:01:24 +0100

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,23 +14,25 @@ status_codes = {
     '405': 0,
     '500': 0
 }
+line_count = 0
 
 try:
-    for i, line in enumerate(sys.stdin, 1):
+    for line in sys.stdin:
+        line_count += 1
         line_data = re.search(r'(\d+.\d+.\d+.\d+)\s-\s\[(.*?)\]\s"GET\s\/projects\/260\sHTTP\/1.1"\s(\d+)\s(\d+)', line)
         if line_data:
             ip_address, date, status_code, file_size_line = line_data.groups()
             file_size += int(file_size_line)
             status_codes[status_code] += 1
 
-        if i % 10 == 0:
+        if line_count % 10 == 0:
             print(f'File size: {file_size}')
-            for code, count in sorted(status_codes.items()):
-                if count > 0:
-                    print(f'{code}: {count}')
+            for code in sorted(status_codes.keys()):
+                if status_codes[code] > 0:
+                    print(f'{code}: {status_codes[code]}')
 
 except KeyboardInterrupt:
     print(f'File size: {file_size}')
-    for code, count in sorted(status_codes.items()):
-        if count > 0:
-            print(f'{code}: {count}')
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f'{code}: {status_codes[code]}')
