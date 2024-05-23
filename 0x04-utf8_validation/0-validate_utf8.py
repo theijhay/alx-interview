ab070de93b15a90d1d179a37bd2a806ecf50ac7d
theijhay <olawaleisaacjohn@gmail.com>
2024-05-23 14:35:28 +0100

    A method that determines if a given data set represents a valid UTF-8 encoding.

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation module
+"""
+
+def validUTF8(data):
+    """
+    Determine if a given data set represents a valid UTF-8 encoding.
+    
+    Args:
+        data (list of int): The data set to validate.
+    
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else False.
+    """
+    num_bytes = 0
+
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
+
+    for num in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+            while mask & num:
+                num_bytes += 1
+                mask >>= 1
+            
+            if num_bytes == 0:
+                continue
+            
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+        
+        num_bytes -= 1
+    
+    return num_bytes == 0
