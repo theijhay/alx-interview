929df54bd33a43172983208a8b53fbcd01163614
theijhay <olawaleisaacjohn@gmail.com>
2024-05-23 21:19:16 +0100

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""
-UTF-8 Validation
-"""
+"""utf-8 validation module"""
 
 
-def validUTF8(data) -> bool:
-    """
-    Returns True if data is a valid UTF-8 encoding, else return False
-    :param data:
-    :return:
-    """
-    num_bytes = 0
-    for byte in data:
-        mask = 1 << 7
-        if not num_bytes:
-            while byte & mask:
-                num_bytes += 1
-                mask >>= 1
-            if not num_bytes:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
+def validUTF8(data):
+    """Determines if a given data is a valid UTF-8 encoding"""
+    byte_counter = 0
+    # To count the number of bytes in the current UTF-8 character
+
+    for a in data:
+        if byte_counter == 0:
+            # Check the first byte of a UTF-8 character
+            if a >> 5 == 0b110:  # 2-byte character (starts with 110xxxxx)
+                byte_counter = 1
+            elif a >> 4 == 0b1110:  # 3-byte character (starts with 1110xxxx)
+                byte_counter = 2
+            elif a >> 3 == 0b11110:  # 4-byte character (starts with 11110xxx)
+                byte_counter = 3
+            elif a >> 7 == 0b1:
+                # Invalid if it starts with 1xxxxxxx (single byte starts by 0)
                 return False
         else:
-            if byte >> 6 != 0b10:
+            # Check the subsequent bytes which should be of the form 10xxxxxx
+            if a >> 6 != 0b10:
                 return False
-        num_bytes -= 1
-    return num_bytes == 0
+            byte_counter -= 1
+
+    # If we have processed all bytes, byte_count should be zero
+    return byte_counter == 0
